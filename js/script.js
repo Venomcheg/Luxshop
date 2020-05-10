0fd721da1b39ca130cfac45f92a0e6cc53caa025
Nikita Fidyk <niknamenik@mail.ru>
2020-05-10 11:09:37 +0700

    Complete header

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
-$(function(){
-	$('.header-slider__list').slick({
-	  dots: true,
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="img/next-arrow.svg" alt="next arrow"></button>',
-	  prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="img/prev-arrow.svg" alt="prev arrow"></button>',
-	  responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	      	arrows: true
-	      }
-	    },
-	    {
-	      breakpoint: 640,
-	      settings: {
-	      	arrows: false
-	      }
-	    }
-	  ]
-	});
+let user_icon = document.querySelector(".user-header__icon");
+user_icon.addEventListener("click", function (e) {
+	let user_menu = document.querySelector(".user-header__menu");
+	user_menu.classList.toggle("_active");
 });
