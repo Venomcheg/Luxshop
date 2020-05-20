bd84f80d8d9cc77b8b16349f3584f13f0b14e6b6
Nikita Fidyk <niknamenik@mail.ru>
2020-05-20 09:41:35 +0700

    Start slider

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-function body_lock(delay) {
-	var body = document.querySelector("body");
-
-	if (body.classList.contains('_lock')) {
-		body_lock_remove(delay);
-	} else {
-		body_lock_add(delay);
-	}
-}
-
 function body_lock_remove(delay) {
 	var body = document.querySelector("body");
 
@@ -76,3 +66,70 @@ function menu_close() {
 	menuBody.classList.remove("_active");
 }
 
+document.documentElement.addEventListener('click', function (e) {
+	if (!e.target.closest(".user-header")) {
+		let user_menu = document.querySelector('.user-header__menu');
+		user_menu.classList.remove('_active');
+	}
+})
+let sliders = document.querySelectorAll("._swiper");
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if (!slider.classList.contains('swiper-bild')) {
+			let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+			let slider_content = slider.innerHTML;
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add("swiper-bild");
+		}
+		if (slider.classList.contains('_gallery')) {
+			//slider.data('lightGallery').destroy(true);
+
+		}
+	}
+	sliders_bild_callback();
+}
+function sliders_bild_callback(params) { }
+
+let main_slider = new Swiper('.main-slider__body', {
+	/*
+	effect: 'fade',
+	autoplay: {
+	delay: 3000,
+	disableOnInteraction: false,
+	},
+	*/
+	observer: true,
+	observeParents: true,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	//autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	loop: true,
+	//preloadImages: false,
+	//lazy: true,
+	// Dotts
+	//pagination: {
+	//	el: '.slider-quality__pagging',
+	//	clickable: true,
+	//},
+	// Arrows
+	navigation: {
+		nextEl: '.control-main-slider__arrow_next',
+		prevEl: '.control-main-slider__arrow_prev',
+
+	},
+
+});
